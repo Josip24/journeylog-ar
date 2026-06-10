129887788e1d84989bd19fa9c741a525969ecef6
Josip <josip0dominkovic@gmail.com>
2026-06-10 14:33:44 +0200

    feat: initial project setup

---
+++ waypoints.js
@@ -0,0 +1,6 @@
+const WAYPOINTS = [
+  { name: 'Mensa',          icon: '🍽',  lat: 48.74190, lng: 9.10010 },
+  { name: 'Würfel',         icon: '🎲',  lat: 48.74150, lng: 9.10050 },
+  { name: 'Vorlesungssaal', icon: '🎓',  lat: 48.74170, lng: 9.09950 },
+  { name: 'Lernwelt',       icon: '📚',  lat: 48.74220, lng: 9.10020 },
+];
